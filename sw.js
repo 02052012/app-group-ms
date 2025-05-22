fd39a59600975561a60f6401b7804bc2a429ba51
02052012 <sandroscherruth@gmail.com>
2025-05-22 13:29:02 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+
+self.addEventListener('install', function(e) {
+    console.log('Service Worker: Installed');
+    e.waitUntil(
+        caches.open('app').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/manifest.json',
+                '/style.css'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
